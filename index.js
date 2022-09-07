d198e3f7a987fa2a3d8dc3b95430bcf22f27384a
chenghui <603094765@qq.com>
2022-09-07 11:04:40 +0800

    u

--- index.js
+++ index.js
@@ -3,68 +3,69 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       73
+// @author       02的狗
 // @match        *://*.baidu.com/*
-// @icon         https://www.baidu.com/favicon.ico
+// @match        *://*.zhihu.com/*
+// @icon         https://img2.baidu.com/it/u=3969377962,2359772706&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1662656400&t=4127b5412921f05aff87a8eb927c7f70
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
 
 /**
- * 百度去广告
+ * 写在前面：通过`@run-at`初始化嵌入css样式来实现功能，提升页面浏览平滑度(好像也就是比起其他的徐晃一下看起来好一点点...)
+ *          不考录兼容，反正只有我在用^_^
+ *
+ * 1.百度列表去广告
+ * 2.知乎列表去广告和视频话题：
+ *    列表视频需Experimental Web Platform features 以支持css`has`选择器体验 ==> chrome://flags
  */
 
-(function () {
+ (function () {
   'use strict';
   /**datas */
   const datas = {
-    advertise: [
-      { funcName: 'advertise', name: 'baidu', match: /^https?:\/\/[a-z]+\.baidu\.com/ }
+    /**css样式嵌入 */
+    cssInsert: [
+      {
+        funcName: 'cssInsert',
+        name: 'baidu',
+        match: /^https?:\/\/[a-z]+\.baidu\.com/,
+        /**
+         * #content_left > :not(.xpath-log, .hit-toptip) 列表页广告
+         * #content_right 右侧热点
+         */
+        css: `
+          #content_left > :not(.xpath-log, .hit-toptip),
+          #content_right {
+            display: none;
+          }
+        `
+      },
+      {
+        funcName: 'cssInsert',
+        name: 'zhihu',
+        match: /^https?:\/\/[a-z]+\.zhihu\.com/,
+        /**
+         * .Topstory-recommend .TopstoryItem--advertCard 列表广告
+         * .Sticky .GlobalSideBar-category, .css-173vipd 右侧个人信息部分功能
+         * .Pc-card 右侧广告
+         * .TopstoryItem:has(iframe) 视频类型话题
+         */
+        css: `
+          .Topstory-recommend .TopstoryItem--advertCard,
+          .Sticky .GlobalSideBar-category,
+          .css-173vipd,
+          .Pc-card,
+          .TopstoryItem:has(iframe) {
+            display: none;
+          }
+        `
+      },
     ],
-  }
-  var { advertise } = datas;
-  const businessLogic = new BusinessLogic()
-  /**功能判断 */
-  ([...advertise]).forEach(item => {
-    if (location.href.match(item.match)) {
-      switch (item.funcName) {
-        case 'advertise':
-          businessLogic.advertiseFun(item.name)
-          break;
+    /**页面优化 */
+    optimization: [
 
-        default:
-          break;
-      }
-    }
-  })
-
-  /**
-   * 业务判断
-   */
-  class BusinessLogic extends OperateDOM {
-    /**
-     * @param { string } name
-     * @return { 去广告样式表判断 }
-     */
-    advertiseFun(name) {
-      let setStyle;
-      switch (name) {
-        case 'baidu':
-          setStyle = `
-            #content_left > :not(.xpath-log, .hit-toptip) {
-              display: none !important;
-            }
-            #content_right {
-              display: none !important;
-            }
-          `
-          break;
-  
-        default:
-          break;
-      }
-      this.CSSinsert(setStyle)
-    }
+    ]
   }
 
   /**
@@ -81,5 +82,22 @@
       document.querySelector('html').appendChild(domStyle);
     }
   }
+
+  const { cssInsert, optimization } = datas;
+  const operateDOM = new OperateDOM()
+  /**功能判断 */
+  const dataAll = [...cssInsert, ...optimization]
+  dataAll.forEach(item => {
+    if (location.href.match(item.match)) {
+      switch (item.funcName) {
+        case 'cssInsert':
+          operateDOM.CSSinsert(item.css)
+          break;
+
+        default:
+          break;
+      }
+    }
+  })
 })();
 
